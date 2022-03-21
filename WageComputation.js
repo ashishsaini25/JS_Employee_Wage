77a33f4336274773cacca4587eb421492b15afb4
ghost331 <67087070+ghost331@users.noreply.github.com>
2022-03-21 22:28:53 +0530

    [Ashsih] Add: Employee Attendence Problem Solved

---
+++ WageComputation.js
@@ -0,0 +1,11 @@
+{ 
+    const Is_Present=1;
+    let check=Math.floor(Math.random()*10)%2;
+    if(check==Is_Present)
+    {
+       console.log("Employee is present");
+    }
+else{
+    console.log("Employee is absent");
+    }
+}
