274c4cd121094deb259222726909fdbf63a7d00e
ghost331 <67087070+ghost331@users.noreply.github.com>
2022-03-25 08:18:31 +0530

    [Ashish] Add:finding the days when employee is present full day half day or absent

--- WageComputation.js
+++ WageComputation.js
@@ -4,6 +4,9 @@
     const full_time_hour=8;
     const half_time_hour=4;
     const wage_per_hour=20;
+    const Maximum_Working_hour=160;
+    let empdailywagemap =new Map();
+    let empdailyhrsmap =new Map();
     let dailywagerecode= new Array();
     function getWorkinghour(check){
         if(check==full_time)return full_time_hour;
@@ -17,21 +20,38 @@
     function DailyWage(value){
         console.log(value);
     }
-    let net_income=0;
     let total_time=0;
-    for(let i=0;i<20;i++){
+    for(let i=1;i<=20;i++){
     let check=Math.floor(Math.random()*10)%3;
     let curr_working_hour=getWorkinghour(check);
-
-    if(total_time+curr_working_hour<160){
+    if(total_time+curr_working_hour<Maximum_Working_hour){
     dailywagerecode.push(Calculatedailywage(curr_working_hour));
+    empdailywagemap.set(i,Calculatedailywage(curr_working_hour));
+    empdailyhrsmap.set(i,curr_working_hour);
     total_time+=curr_working_hour;
-    net_income+=Calculatedailywage(curr_working_hour);
     }
     }
-    dailywagerecode.forEach(DailyWage);
-    console.log("The net income of the employee is :"+net_income + " the total working hour of the employee is :"+total_time);
 
+    let full_time_working_day= new Array();
+    let half_time_working_day= new Array();
+    let absent_on_the_Day =new Array();
+    empdailyhrsmap.forEach((value,key,map)=>{
+        if(value==8)full_time_working_day.push(key);
+        else if(value==4)half_time_working_day.push(key);
+        else
+        absent_on_the_Day.push(key);
 
+    });
+    console.log("Full day ");
+    full_time_working_day.forEach(DailyWage);
+
+    console.log("half day ");
+     console.log(half_time_working_day);
+    
+
+    console.log(" absent ");
+   absent_on_the_Day.forEach(DailyWage);
+
+    
 }
 
