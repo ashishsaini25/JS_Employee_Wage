6dfd58edfb8d0f43b6386a942a2a5373d3db1198
ghost331 <67087070+ghost331@users.noreply.github.com>
2022-03-25 09:26:38 +0530

    [Ashish] Add: Obejct created which will strore all the information

--- WageComputation.js
+++ WageComputation.js
@@ -21,37 +21,25 @@
         console.log(value);
     }
     let total_time=0;
-    for(let i=1;i<=20;i++){
+
+let empinfo=new Array();
+for(let i=1;i<=20;i++){
     let check=Math.floor(Math.random()*10)%3;
     let curr_working_hour=getWorkinghour(check);
     if(total_time+curr_working_hour<Maximum_Working_hour){
-    dailywagerecode.push(Calculatedailywage(curr_working_hour));
-    empdailywagemap.set(i,Calculatedailywage(curr_working_hour));
-    empdailyhrsmap.set(i,curr_working_hour);
+      empinfo.push(
+          {
+              daynum:i,
+              dayhours:curr_working_hour,
+              daywage:Calculatedailywage(curr_working_hour),
+          }
+      );
     total_time+=curr_working_hour;
     }
     }
+    console.log(empinfo);
+   
 
-    let full_time_working_day= new Array();
-    let half_time_working_day= new Array();
-    let absent_on_the_Day =new Array();
-    empdailyhrsmap.forEach((value,key,map)=>{
-        if(value==8)full_time_working_day.push(key);
-        else if(value==4)half_time_working_day.push(key);
-        else
-        absent_on_the_Day.push(key);
 
-    });
-    console.log("Full day ");
-    full_time_working_day.forEach(DailyWage);
-
-    console.log("half day ");
-     console.log(half_time_working_day);
-    
-
-    console.log(" absent ");
-   absent_on_the_Day.forEach(DailyWage);
-
-    
 }
 
